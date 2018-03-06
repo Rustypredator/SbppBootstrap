18f22fe0fe6c1e338db8fd694fa56449e929b70c
Rustypredator <contact@rusty.info>
2018-03-06 13:57:45 +0100

    Added Style/js/fonts files, added config

---
+++ theme.conf.php
@@ -0,0 +1,36 @@
+<?php
+/*************************************************************************
+
+Sourcebans++ Bootstrap Theme
+Copyright (C) 2018  Rustypredator (https://github.com/rustypredator)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ *************************************************************************/
+
+// Set the name of this theme here
+define('theme_name', "SourceBans++ Bootstrap Theme");
+
+// Set the author of this theme here
+define('theme_author', "Rustypredator");
+
+// Set the version of the theme here
+define('theme_version', "0.0.1");
+
+// Set the link of the theme here
+define('theme_link', "https://github.com/Rustypredator/SbppBootstrap");
+
+// Set the screenshot filename for your theme (must be inside your theme folder)
+// Must be:  250px wide  X 170px High
+define('theme_screenshot', "style/images/preview.jpg");
